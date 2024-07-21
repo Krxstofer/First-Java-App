c810a436913b322fd2a41b85be584ffa81a2c669
Kristofer Norman <kjn2418@email.vccs.edu>
2024-07-21 03:32:03 -0400

    Creating my first java application that allows a user to type there name int he console and display it.

--- MainClass.java
+++ MainClass.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class MainClass {
     public static void main (String[] Args) {
         System.out.println("Hello World!");
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = scan.nextLine();
+        System.out.println("Hello, " + name);
+        scan.close();
     }
 }
 
